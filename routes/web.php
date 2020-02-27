7077ba41a3d2d8874672e6877552f9028bafe8a9
leandro-web <leandro-web@hotmail.com>
2020-02-27 00:47:15 -0300

    Test Route

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empresa', function () {
+    return 'Sobre a empresa';
+});
+
+Route::get('contato', function () {
+    return 'contato da empresa';
+});
