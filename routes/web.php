5d9e5a7b7025799360bc2d44a993aa16783d6844
leandro-web <leandro-web@hotmail.com>
2020-03-02 17:40:00 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('login', function () {
+    return 'login';
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,5 +13,39 @@ Route::get('/empresa', function () {
 });
 
 Route::get('contato', function () {
-    return 'contato da empresa';
+    return view('contato');
+});
+
+Route::get('produtos/{id?}', function ($id = '') {
+    return "lista de produtos do id: {$id}";
+});
+
+Route::get('categorias/{param_1}', function($param_1) {
+    return "Produtos da categoria: {$param_1}";
+});
+
+Route::get('redirect1', function () {
+    return redirect('redirect2');
+});
+
+Route::get('redirect2', function () {
+    return 'redirect2';
+}); 
+/*
+Route::get('admin/dashboard', function () {
+    return 'Home admin';
+})->middleware('auth');
+*/
+Route::middleware([])->prefix('admin')->group(function(){
+    Route::get('dashboard', function () {
+        return 'Home admin';
+    });
+    
+    Route::get('financeiro', function () {
+        return 'Admin do financeiro';
+    });
+    
+    Route::get('produtos', function () {
+        return 'Admin dos produtos';
+    });
 });
